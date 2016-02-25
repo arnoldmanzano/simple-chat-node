4402bec8c5a0af8313360caa9523897138bedfa9
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-25 21:21:37 +0000

    Improve test code for multiremote browsers

--- spec/features/chatting_spec.js
+++ spec/features/chatting_spec.js
@@ -1,14 +1,14 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var WebdriverIO = require('webdriverio'),
-    matrix = WebdriverIO.multiremote({
-        browserA: { desiredCapabilities: { browserName: 'chrome' } },
-        browserB: { desiredCapabilities: { browserName: 'chrome' } }
-    }),
-    browserA = matrix.select('browserA'),
-    browserB = matrix.select('browserB');
+var WebdriverIO = require('webdriverio');
+var matrix = WebdriverIO.multiremote({
+  browserA: { desiredCapabilities: { browserName: 'chrome' } },
+  browserB: { desiredCapabilities: { browserName: 'chrome' } }
+});
 
+var browserA = matrix.select('browserA');
+var browserB = matrix.select('browserB');
 
 describe('Chatting', function() {
 
@@ -20,11 +20,12 @@ describe('Chatting', function() {
 
       browserB
         .init().url('http://localhost:3000')
-        .pause(500)
+        .pause(100)
         .waitForExist('.ind_msg', 5000)
         .getText('body', function(err, text) {
-          expect(text).to.include('My name is Arnold');
-        })
-        .call(done);
+          expect(text).to.include('My name is Hello');
+        });
+
+      matrix.sync().call(done).end();
   });
 });
